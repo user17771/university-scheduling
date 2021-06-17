b826f4b2a7ed08c321fdefba641154390a5add25
Nima <nimip0344@gmail.com>
2021-06-17 19:39:07 +0430

    Adding Readme

---
+++ README.md
@@ -0,0 +1,17 @@
+OK, listen mate
+
+Go inside the repo folder
+
+then do this :
+
+npm install .
+
+done
+
+now if you want to run it:
+
+node .
+
+and don't forget to have a bloody mongodb instance running on your local computer
+
+best of luck (No)
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
+const MONGOOSE_OPTS = { useNewUrlParser: true, useUnifiedTopology: true }
 
-mongoose.connect('mongodb://localhost/users')
+mongoose.connect('mongodb://localhost/users', MONGOOSE_OPTS)
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
